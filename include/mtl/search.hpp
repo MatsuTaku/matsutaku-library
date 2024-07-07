9b6e408cbe5b8e5e8d323ae3f8f3d6c87a1e56fa
MatsuTaku <tkm.matsumoto@gmail.com>
2024-07-08 00:18:04 +0900

    Refactor interfaces in search.hpp

--- include/mtl/search.hpp
+++ include/mtl/search.hpp
@@ -34,55 +34,79 @@ bisect_float(double ok, double ng, F fn, double eps = EPS_DEFAULT) {
   return ok;
 }
 
-template<typename T, typename I, typename F, typename C>
-std::pair<typename std::remove_reference<I>::type, T>
-fibonacci_search(I l, I r, F fn, C cmp = std::less<T>()) {
-  using idx_type = typename std::remove_reference<I>::type;
-  assert(r - l >= 2);
-  idx_type d = r-l;
-  std::vector<idx_type> fib{1,1};
-  while (fib.back() < d) fib.push_back(fib[fib.size()-2] + fib[fib.size()-1]);
-  auto k = --fib.cend();
-  idx_type il = l + *(k-2);
-  idx_type ir = l + *(k-1);
-  T lv = fn(il);
-  T rv = fn(ir);
-  std::pair<idx_type, T> ret = cmp(lv, rv) ? std::make_pair(ir, rv) : std::make_pair(il, lv);
-  while (*k > 3) {
-    if (cmp(lv, rv)) {
-      l += *(k-2);
-      lv = rv;
-      auto i = std::min(r-1, l + *(k-2));
-      rv = fn(i);
-      if (cmp(ret.second, rv))
-        ret = std::make_pair(i, rv);
-    } else {
-      rv = lv;
-      auto i = std::min(r-1, l + *(k-3));
-      lv = fn(i);
-      if (cmp(ret.second, lv))
-        ret = std::make_pair(i, lv);
-    }
-    --k;
+template<class Cmp = std::less<>>
+class FibonacciSearch {
+ public:
+  using idx_type = long long;
+ private:
+  std::vector<long long> fib_;
+  Cmp cmp_;
+  void setup_fib(long long max_distance) {
+    while (fib_.back() < max_distance) fib_.push_back(fib_[fib_.size()-2] + fib_[fib_.size()-1]);
   }
-  return ret;
+ public:
+  FibonacciSearch(long long max_distance = 1, const Cmp& cmp = Cmp()) : fib_{1,1}, cmp_(cmp) {
+    setup_fib(max_distance);
+  }
+
+  template<class F>
+  auto operator()(idx_type l, idx_type r, F fn) const 
+    -> std::pair<idx_type, decltype(fn(l))> 
+  {
+    assert(r - l >= 2);
+    idx_type d = r-l;
+    setup_fib(d);
+    auto k = --fib_.cend();
+    idx_type il = l + *(k-2);
+    idx_type ir = l + *(k-1);
+    auto lv = fn(il);
+    auto rv = fn(ir);
+    auto ret = cmp(lv, rv) ? std::make_pair((idx_type)ir, rv) : std::make_pair((idx_type)il, lv);
+    while (*k > 3) {
+      if (cmp(lv, rv)) {
+        l += *(k-2);
+        lv = rv;
+        auto i = std::min(r-1, l + *(k-2));
+        rv = fn(i);
+        if (cmp(ret.second, rv))
+          ret = std::make_pair(i, rv);
+      } else {
+        rv = lv;
+        auto i = std::min(r-1, l + *(k-3));
+        lv = fn(i);
+        if (cmp(ret.second, lv))
+          ret = std::make_pair(i, lv);
+      }
+      --k;
+    }
+    return ret;
+  }
+};
+
+template<typename I, typename F, typename C = std::less<>>
+auto fibonacci_search(I l, I r, F fn, const C& cmp = C()) 
+  -> std::pair<typename std::remove_reference<I>::type,
+               decltype(fn(l))> {
+  return FibonacciSearch(r - l, cmp)(l, r, fn);
 }
-template<typename T, typename I, typename F, typename C = std::less<T>>
-std::pair<typename std::remove_reference<I>::type, T>
-trisect_int(I l, I r, F fn, C cmp = C()) {
-  return fibonacci_search<T>(l, r, fn, cmp);
+template<typename I, typename F, typename C = std::less<>>
+auto trisect_int(I l, I r, F fn, const C& cmp = C())
+  -> std::pair<typename std::remove_reference<I>::type, 
+               decltype(fn(l))> {
+  return fibonacci_search(l, r, fn, cmp);
 }
 
 constexpr double GOLDEN_RATIO = 1.61803398875;
-template<typename T, typename F, typename C>
-std::pair<double, T>
-golden_ratio_search(double l, double r, F fn, double eps = EPS_DEFAULT, C cmp = std::less<T>()) {
-  std::pair<double, T> ret;
+template<typename F, typename C = std::less<>>
+auto golden_ratio_search(double l, double r, F fn, double eps = EPS_DEFAULT, const C& cmp = C())
+  -> std::pair<double, decltype(fn(l))> {
+  using value_type = decltype(fn(l));
+  std::pair<double, value_type> ret;
   while (std::abs(r - l) > eps) {
     double il = (l * GOLDEN_RATIO + r) / (GOLDEN_RATIO + 1.);
     double ir = (l + r * GOLDEN_RATIO) / (GOLDEN_RATIO + 1.);
-    T vl = fn(il);
-    T vr = fn(ir);
+    auto vl = fn(il);
+    auto vr = fn(ir);
     if (cmp(vr, vl)) {
       ret = std::make_pair(il, vl);
       r = ir;
@@ -93,8 +117,8 @@ golden_ratio_search(double l, double r, F fn, double eps = EPS_DEFAULT, C cmp = 
   }
   return ret;
 }
-template<typename T, typename F, typename C>
-std::pair<double, T>
-trisect_float(double l, double r, F fn, double eps = EPS_DEFAULT, C cmp = std::less<T>()) {
-  return golden_ratio_search<T>(l, r, fn, eps, cmp);
+template<typename F, typename C = std::less<>>
+auto trisect_float(double l, double r, F fn, double eps = EPS_DEFAULT, const C& cmp = C())
+  -> std::pair<double, decltype(fn(l))> {
+  return golden_ratio_search(l, r, fn, eps, cmp);
 }
