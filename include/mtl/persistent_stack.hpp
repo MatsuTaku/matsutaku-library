78a5c7943416908344ca6061e50a662a9f89539d
MatsuTaku <tkm.matsumoto@gmail.com>
2024-07-07 23:57:13 +0900

    Refactor interfaces of PersistentStack

--- include/mtl/persistent_stack.hpp
+++ include/mtl/persistent_stack.hpp
@@ -6,14 +6,17 @@
 template<typename T>
 class PersistentStack {
  public:
+  using value_type = T;
+  using const_reference = const value_type&;
+  using reference = const_reference;
   struct Node;
   using node_ptr = std::shared_ptr<Node>;
   struct Node {
-    T v;
+    value_type v;
     node_ptr next;
     Node() = default;
-    Node(const T& v, node_ptr next) : v(v), next(next) {}
-    Node(T&& v, node_ptr next) : v(std::move(v)), next(next) {}
+    Node(const value_type& v, node_ptr next) : v(v), next(next) {}
+    Node(value_type&& v, node_ptr next) : v(std::move(v)), next(next) {}
   };
  private:
   node_ptr top_;
@@ -23,7 +26,7 @@ class PersistentStack {
   PersistentStack() : top_(nullptr), size_(0) {}
   size_t size() const { return size_; }
   bool empty() const { return size() == 0; }
-  const T& top() const { return top_->v; }
+  const_reference top() const { return top_->v; }
  private:
   template<typename V>
   PersistentStack _push(V&& v) const {
@@ -31,15 +34,19 @@ class PersistentStack {
     return PersistentStack(new_node, size()+1);
   }
  public:
+  template<class... Args>
+  [[nodiscard]] PersistentStack emplace(Args&&... args) const {
+    return _push(value_type(std::forward<Args>(args)...));
+  }
   template<typename V>
   [[nodiscard]] PersistentStack push(V&& v) const {
-    static_assert(std::is_convertible<V,T>::value, "");
+    static_assert(std::is_convertible<V,value_type>::value, "");
     return _push(std::forward<V>(v));
   }
-  [[nodiscard]] PersistentStack push(const T& v) const {
+  [[nodiscard]] PersistentStack push(const value_type& v) const {
     return _push(v);
   }
-  [[nodiscard]] PersistentStack push(T&& v) const {
+  [[nodiscard]] PersistentStack push(value_type&& v) const {
     return _push(std::move(v));
   }
   [[nodiscard]] PersistentStack pop() const {
